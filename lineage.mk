20f06314c0a60afaf14fbd0a7607f7ee559901ff
troufiniou <troufiniou@hotmail.fr>
2017-04-28 19:51:19 +0200

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -3,16 +3,16 @@ $(call inherit-product, device/xiaomi/natrium/full_natrium.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_natrium
-PRODUCT_DEVICE := natrium
+PRODUCT_NAME := lineage_scorpio
+PRODUCT_DEVICE := scorpio
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := MI 5s Plus
+PRODUCT_MODEL := MI Note 2
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE="natrium" \
-    PRODUCT_NAME="natrium" \
-    BUILD_FINGERPRINT="Xiaomi/natrium/natrium:6.0.1/MXB48T/V8.2.2.0.MBGMIDL:user/release-keys" \
-    PRIVATE_BUILD_DESC="natrium-user 6.0.1 MXB48T V8.2.2.0.MBGMIDL release-keys"
+    TARGET_DEVICE="scorpio" \
+    PRODUCT_NAME="scorpio" \
+    BUILD_FINGERPRINT="Xiaomi/scorpio/scorpio:6.0.1/MXB48T/V8.2.2.0.MBGMIDL:user/release-keys" \
+    PRIVATE_BUILD_DESC="scorpio-user 6.0.1 MXB48T V8.2.5.0.MADCNDL release-keys"
